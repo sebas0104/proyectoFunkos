f1a50b11123495adbf26892917c6c4bb33c05d7d
sebastian <sebastian.am@hotmail.com>
2021-11-01 17:42:53 -0500

    corregir  un  error

--- src/main/java/com/FunkoStore/model/Usuario.java
+++ src/main/java/com/FunkoStore/model/Usuario.java
@@ -4,9 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Entity
 @Table(name = "tb_usuario")
